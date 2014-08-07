3ed5ce79a566f8d2bf9d9e2608c7dc5bbdbb2b45
eunicekokor <eunicekokor@gmail.com>
2014-08-07 14:59:14 -0700

    such methods much code design

--- Java/Problem4.java
+++ Java/Problem4.java
@@ -1,32 +1,52 @@
 public class Problem4{
 
-     public static void main(String []args){
-
-     int x = 0;
-     int pal = 0;
+    public static void main(String []args){
+    int lower = 100;
+    int upper = 1000;
+    System.out.println(getMaxPal(lower, upper));
+    }
     
-      for (int i = 100; i < 1000; i++){
-        for (int j = 100; j < 1000; j++){
-            x = i*j;
-            int num = x; //reverse number algo
-            int rev = 0;
-            int temp = 0;
+    public static int getMaxPal(int num1, int num2){
+    int x = 0;
+    int pal = 0;
+    int count = 0;
+    //x is continuously defined as the product of i&j
+    //pal is the greatest palindrome
+    
+        for (int i = num1; i < num2; i++){
+            for (int j = num1; j < num2; j++){
+                x = i*j;
+                
+                if (isPal(x,getRev(x))){
+                    //trying to count efficiency
+                    count++;
+                    if (x > pal) {
+                        pal = x; //stores value of largest palindrome
+                    }
+                }
+            }
+        }
+    //System.out.println(count);
+    return pal;
+    }
+    
+    //reversing the number
+    public static int getRev(int number){
+        int num = number;
+        int rev = 0;
+        int temp = 0;
             while (num > 0){
                 temp = (num)%10;
                 rev = rev * 10 + temp;
                 num = num/10;
             }
-            
-            if (x == rev){ //for sure a palindrome
-                if (x > pal) {
-                    pal = x; //stores value of largest palindrome
-                }
-            }
-        }    
+    return rev;
     }
-    System.out.println(pal);
-    /*note, can create isPal method*/
-
-
-     }
+    
+    public static boolean isPal(int num1, int num2){
+        if (num1 == num2){
+            return true;
+        }
+        else return false;
+    }
 }
